6927097dbfef98cd9f885e7c08213c1f98cab0da
José GUilherme <jose.guilherme96@outlook.com.br>
2025-11-18 18:05:51 -0300

    adiciona script que gera esquema do banco de dados

---
+++ api/src/schema/generate_schema.py
@@ -0,0 +1,41 @@
+from sqlalchemy import MetaData
+from sqlalchemy_schemadisplay import create_schema_graph
+from __init__ import create_app
+import os
+from dotenv import load_dotenv
+
+def generate_schema():
+
+    try:
+        load_dotenv(dotenv_path="api/.env")
+        app, db = create_app()
+
+        with app.app_context():
+
+            db.create_all()
+            metadata = MetaData()
+            metadata.reflect(bind=db.engine)
+            
+            graph = create_schema_graph(
+                db.engine,     
+                metadata=metadata,
+                show_datatypes=False,
+                show_indexes=False,
+                rankdir='LR',
+                concentrate=False
+            )
+
+            path_image_schema_bd = os.getenv("path_image_schema_bd")
+            path_file_save_svg = f"{os.getcwd()}/{path_image_schema_bd}/db.svg"
+            graph.write_svg(f"{path_image_schema_bd}/db.svg")
+            db.engine.dispose()
+            print(f"O schema foi gerado com sucesso ! {path_file_save_svg}")
+
+    except Exception as e:
+
+        print(f"Falha ao gerar schema : {e}")
+
+
+if __name__ == "__main__":
+
+    generate_schema()
---
+++ api/tests/unit/test_generate_schema.py
@@ -0,0 +1,20 @@
+from __init__ import create_app
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+from schema.generate_schema import generate_schema
+
+def test_deve_criar_schema_banco_de_dados(capfd,criar_banco_de_dados):
+
+    generate_schema()
+
+    load_dotenv(dotenv_path="api/.env")
+    path_image_schema_bd = os.getenv("path_image_schema_bd")
+    path = Path(path_image_schema_bd)
+
+    imagens = [imagem for imagem in path.iterdir() if imagem.suffix == '.svg']
+    assert imagens[0].name == "db.svg"
+
+    os.remove(f"{path}/{imagens[0].name}")
+
+    
