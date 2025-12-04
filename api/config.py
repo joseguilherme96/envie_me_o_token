2cf8f43f27d1fd5a1e887a8614e00044ec3a98c2
José GUilherme <jose.guilherme96@outlook.com.br>
2025-12-04 11:19:57 -0300

    adicionado condição para setar automaticamente ambiente development caso não seja configurado

--- api/config.py
+++ api/config.py
@@ -1,6 +1,13 @@
 
 from dynaconf import Dynaconf
 from pathlib import Path
+import os
+
+environment = os.getenv("ENV_FOR_DYNACONF",None)
+
+if environment is None:
+
+    os.environ["ENV_FOR_DYNACONF"] = "development"
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
