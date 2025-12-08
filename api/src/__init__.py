097e79ef059640a502d76a593e166c23e6889ebb
José GUilherme <jose.guilherme96@outlook.com.br>
2025-12-08 12:00:16 -0300

    alterado mensagens para exibição em logging

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from src.routes.index import main
 from src.routes.v1.enviar_token_paciente.index import token
+from src.routes.v1.tipo_user.index import tipo_user
 from src.models.db import db
 from config import settings
 from dynaconf import FlaskDynaconf
+import os
+import logging
 
 from src.config_migrate import instancia_migrate
 
@@ -14,20 +17,24 @@ def create_app(app_config=None):
 
     app.register_blueprint(main)
     app.register_blueprint(token)
-
-    print("Ambiente Flask :",settings.ENV_FOR_DYNACONF)
-    print("DATABASE_URL :",settings.DATABASE_URL)
-    print("APP_CONFIG :", app_config)
+    app.register_blueprint(tipo_user)
     
     if app_config:
 
+        logging.info(f'Atualizando configuracoes do app....')
         app.config.update(app_config)
+        logging.info(f"Atualizando DATABASE_URL do Dynaconf")
+        settings.configure(DATABASE_URL=app_config["SQLALCHEMY_DATABASE_URI"])
 
     else :
 
         app.config["SQLALCHEMY_DATABASE_URI"] = settings.DATABASE_URL
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    logging.debug(f"ENV_FOR_DYNACONF : {settings.ENV_FOR_DYNACONF}")
+    logging.debug(f"DATABASE_URL : {settings.DATABASE_URL}")
+    logging.debug(f"APP_CONFIG : {app_config}")
 
     db.init_app(app)
 
@@ -39,3 +46,4 @@ def get_db_path():
 
     return settings.DATABASE_URL
 
+ 
