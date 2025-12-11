1c5a0a2464a3bbc2735a86be2b2e8fb280758718
José GUilherme <jose.guilherme96@outlook.com.br>
2025-12-11 10:36:27 -0300

    fix /status endpoint connection bug

--- api/src/routes/v1/status/index.py
+++ api/src/routes/v1/status/index.py
@@ -1,27 +1,49 @@
 
-from flask import Blueprint,jsonify,request
-from src.models.ExecucaoSPSADT import ExecucaoSPSADT
-from src.factory.factory_api_token_paciente import FactoryAPITokenPaciente
-from src.models.UserTipo import TipoUser
+from flask import Blueprint,jsonify
 from src.models.db import db
 from config import settings
 from datetime import datetime
 from src.models.db import db
+from sqlalchemy import text
+import logging
 
 status = Blueprint("status",__name__)
 
 @status.route('/status',methods=["GET"])
-def cadastrar_tipo_user():
-
-    engine = db.get_engine()
-    url = engine.url
+def status_servidor():
 
     try:
+
+        engine = db.get_engine()
+        url = engine.url
+        
+        if url.drivername == "mysql+pymysql":
+
+            execute = db.session.execute(text("SHOW STATUS LIKE 'Threads_connected';"))
+            opened_connetions = execute.fetchone()[1]
+
+            logging.debug(opened_connetions)
+
+            execute = db.session.execute(text("SHOW VARIABLES LIKE 'max_connections';"))
+            max_connections = execute.fetchone()[1]
+
+            logging.debug(max_connections)   
+
+            db.session.close()
+
+        if url.drivername == "sqlite":
+
+            pool = engine.pool
+            opened_connetions = pool.checkedout()
+            max_connections = pool.size()
+
         
         return jsonify({
             "data_hora": datetime.now(),
-            "enviroment": settings.ENV_FOR_DYNACONF,
-            "db": url.database
+            "environment": settings.ENV_FOR_DYNACONF,
+            "db": url.database,
+            "opened_connetions": opened_connetions,
+            "max_connections": max_connections
         }),200
     
     except Exception as e:
--- api/tests/integration/routes/v1/status/test_api_status.py
+++ api/tests/integration/routes/v1/status/test_api_status.py
@@ -8,5 +8,7 @@ def test_api_status_servidor_flask():
 
     response_json = response.json()
 
-    assert response_json["enviroment"] == settings.ENV_FOR_DYNACONF
+    assert response_json["environment"] == settings.ENV_FOR_DYNACONF
     assert response_json["db"].split("\\").pop() == settings.BD_NAME
+    assert response_json['max_connections']
+    assert response_json['opened_connetions'] or response_json['opened_connetions'] == 0
