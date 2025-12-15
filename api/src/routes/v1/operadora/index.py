4795cbd1caf8de357a6b28bb77d61d68c58aaf90
José GUilherme <jose.guilherme96@outlook.com.br>
2025-12-15 13:52:22 -0300

    feat: adds `/operadora` endpoint

---
+++ api/src/routes/v1/operadora/index.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, jsonify, request
+from src.models.Operadora import Operadora
+import logging
+from src.utils.is_numeric_or_error import isNumericOrError
+from src.utils.not_empty_or_error import notEmptyOrError
+
+operadora = Blueprint("operadora", __name__)
+
+@operadora.route('/operadora', methods=["POST"])
+def cadastrar_operadora():
+    try:
+        dados = request.get_json()
+
+        logging.debug(dados)
+        
+        notEmptyOrError(dados, "registro_ans", "O campo registro_ans é obrigatório !")
+        notEmptyOrError(dados, "operadora", "O campo operadora é obrigatório !")
+
+        if(Operadora.buscar({"registro_ans": dados["registro_ans"]})):
+            return jsonify({"message": "Já existe uma operadora com o mesmo registro_ans cadastrado !"}), 409
+        
+        if isinstance(dados.get("operadora"), str) == False:
+            return jsonify({"message": "O campo operadora deve ser um texto !"}), 400
+        
+        isNumericOrError(dados, "registro_ans", "O campo registro_ans deve ser um número inteiro !")
+        
+        dados = Operadora.inserir(Operadora(
+            registro_ans=dados["registro_ans"],
+            operadora=dados["operadora"]
+        ))
+        
+        return jsonify({"message": "Cadastrado com sucesso!", "data": dados}), 201
+    
+    except Exception as e:
+        logging.error(e.args)
+
+        if isinstance(e.args[0], dict):
+            return jsonify({"message": e.args[0]["message"]}), e.args[0]["status_code"]
+
+        return jsonify({"message": f"Falha ao cadastrar! {e}"}), 500
