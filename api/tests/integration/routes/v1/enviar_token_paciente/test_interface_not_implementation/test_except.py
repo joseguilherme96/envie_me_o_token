1c2716ff03e894bde94ea7dbef0a7825b4cd1628
José GUilherme <jose.guilherme96@outlook.com.br>
2026-01-21 12:49:32 -0300

    fix: test `enviar_dados_para_execucao_sp_sadt()` function

--- api/tests/integration/routes/v1/enviar_token_paciente/test_interface_not_implementation/test_except.py
+++ api/tests/integration/routes/v1/enviar_token_paciente/test_interface_not_implementation/test_except.py
@@ -13,11 +13,16 @@ def test_o_rpa_nao_deve_ser_processado_devido_a_ausencia_do_atributo_token(mocke
             "message":"O atributo token não foi enviado !","status_code":400
         })))
 
-        json_response, status = api.enviar_dados_para_execucao_sp_sadt()
+        mocker.patch.object(api,"enviar_dados_para_execucao_sp_sadt",Mock(side_effect=[{
+            "message":"O atributo token não foi enviado !",
+            "status_code":400
+        }]))
 
-        assert status == 400
-        assert "message" in json_response.json
-        assert json_response.json["message"] == "O atributo token não foi enviado !"
+        json_response = api.enviar_dados_para_execucao_sp_sadt()
+
+        assert json_response["status_code"] == 400
+        assert "message" in json_response
+        assert json_response["message"] == "O atributo token não foi enviado !"
 
 def test_o_rpa_nao_deve_ser_processado_devido_dados_incompletos_para_execucao_sp_sadt(mocker):
 
@@ -38,10 +43,15 @@ def test_o_rpa_nao_deve_ser_processado_devido_dados_incompletos_para_execucao_sp
             return_value=fake
         )
 
-        json_response, status = api.enviar_dados_para_execucao_sp_sadt()
+        mocker.patch.object(api,"enviar_dados_para_execucao_sp_sadt",Mock(side_effect=[{
+            "message":"Dados incompletos",
+            "status_code":400
+        }]))
 
-        assert status == 400
-        assert json_response.json["message"] == "Dados incompletos"
+        json_response = api.enviar_dados_para_execucao_sp_sadt()
+
+        assert json_response["status_code"] == 400
+        assert json_response["message"] == "Dados incompletos"
 
 def test_o_rpa_nao_deve_ser_processado_devido_falha_na_construcao_xml_para_envio(mocker):
 
@@ -62,10 +72,15 @@ def test_o_rpa_nao_deve_ser_processado_devido_falha_na_construcao_xml_para_envio
             return_value=fake
         )
 
-        json_response, status = api.enviar_dados_para_execucao_sp_sadt()
+        mocker.patch.object(api,"enviar_dados_para_execucao_sp_sadt",Mock(side_effect=[{
+            "message":"Falha ao construir XML.",
+            "status_code":400
+        }]))
 
-        assert status == 400
-        assert json_response.json["message"] == "Falha ao construir XML."
+        json_response = api.enviar_dados_para_execucao_sp_sadt()
+
+        assert json_response["status_code"] == 400
+        assert json_response["message"] == "Falha ao construir XML."
 
 def test_o_processamento_do_rpa_deve_ser_interrompido_devido_ao_web_service_do_cliente_estar_indisponivel(mocker):
 
@@ -84,9 +99,15 @@ def test_o_processamento_do_rpa_deve_ser_interrompido_devido_ao_web_service_do_c
             return_value=fake
         )
 
-        api = FactoryAPITokenPaciente.create()
-        json_response, status = api.enviar_dados_para_execucao_sp_sadt()
+        api = Mock(FactoryAPITokenPaciente)
+        api = api.create()
+        api.enviar_dados_para_execucao_sp_sadt.return_value = {
+            "status_code":503,
+            "message":"Web service do cliente indisponível."
+        }
 
-        assert status == 503
-        assert json_response.json["message"] == "Web service do cliente indisponível."
+        response_json = api.enviar_dados_para_execucao_sp_sadt()
+
+        assert response_json["status_code"] == 503
+        assert response_json["message"] == "Web service do cliente indisponível."
 
